e5bbee793895759b5e73416bb23ced9ae1ccd6ce
palodavis <psilvadias59@gmail.com>
2025-03-05 16:05:15 -0300

    feat: initial commit

---
+++ ms-event-manager/src/test/java/com/palodavis/ms_event_manager/MsEventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palodavis.ms_event_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ms-ticket-manager/src/test/java/com/palodavis/ms_ticket_manager/MsTicketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palodavis.ms_ticket_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTicketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
